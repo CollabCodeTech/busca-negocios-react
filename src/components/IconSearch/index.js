3e08503392ce20852ebd7c2f38f42ec252a1570b
Marco Bruno <marco.bruno.br@gmail.com>
2020-08-28 23:25:21 +0200

    🚧Create IconSearch

---
+++ src/components/IconSearch/index.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import icon from '../../assets/images/icon-search.svg';
+
+const IconSearch = styled.img.attrs({
+  src: icon,
+  alt: 'Ícone de lupa que represta o recurso de busca no site'
+})``;
+
+export default IconSearch;
