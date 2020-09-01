417c88aafa1af5dbc9e4036f2b6ff5fde3014329
Marco Bruno <marco.bruno.br@gmail.com>
2020-09-01 10:37:08 +0200

    ✨Create IconFilter component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import React from 'react';
 import HeaderMain from './components/HeaderMain';
 import LogoMain from './components/LogoMain';
 import IconSearch from './components/IconSearch';
+import IconFilter from './components/IconFilter';
 
 function App() {
   return (
-    <HeaderMain>
-      <LogoMain />
-      <IconSearch />
-    </HeaderMain>
+    <>
+      <HeaderMain>
+        <LogoMain />
+        <IconSearch />
+      </HeaderMain>
+
+      <IconFilter />
+    </>
   );
 }
 
