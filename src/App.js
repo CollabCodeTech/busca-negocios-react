f0b64c0282b2cd467ab16df6a3ac7fd9c5930c1d
Marco Bruno <marco.bruno.br@gmail.com>
2020-09-01 12:19:51 +0200

    ✨Create ServiceFilter component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import HeaderMain from './components/HeaderMain';
 import LogoMain from './components/LogoMain';
 import IconSearch from './components/IconSearch';
+import { ServiceFilter, Label } from './components/ServiceFilter';
 import IconFilter from './components/IconFilter';
 
 function App() {
@@ -13,7 +14,11 @@ function App() {
         <IconSearch />
       </HeaderMain>
 
-      <IconFilter />
+      <ServiceFilter>
+        <IconFilter />
+
+        <Label>Guia de serviços</Label>
+      </ServiceFilter>
     </>
   );
 }
