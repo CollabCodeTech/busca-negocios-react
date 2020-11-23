61dc5614b4fcbfc5d43181c848477deb2848d720
Marco Bruno <marco.bruno.br@gmail.com>
2020-11-23 13:48:13 +0100

    ✨ Change background of the storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,12 @@
 export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' }
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  backgrounds: {
+    default: 'dark',
+    values: [
+      {
+        name: 'dark',
+        value: '#4c5b84'
+      }
+    ]
+  }
 };
