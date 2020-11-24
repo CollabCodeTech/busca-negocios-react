d20386b8f2a8fd449b7a8c848b16c3951744f791
Marco Bruno <marco.bruno.br@gmail.com>
2020-11-24 15:36:33 +0100

    ✨ Add the ButtonFixed in projetc

---
+++ src/components/ButtonFixed/index.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+const ButtonFixed = styled.button`
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  background-color: #f25a70;
+  font-size: 20px;
+  line-height: 30px;
+  letter-spacing: 0.15px;
+  text-transform: uppercase;
+  height: 50px;
+  border: none;
+  color: #fffcee;
+  box-shadow: 0 -4px 4px rgba(0, 0, 0, 0.25);
+  font-family: 'Comfortaa', sans-serif;
+`;
+
+export default ButtonFixed;
--- src/components/ServiceFilter/styles.js
+++ src/components/ServiceFilter/styles.js
@@ -13,7 +13,7 @@ export const ServiceFilterStyle = styled.nav`
 `;
 
 export const Label = styled.p`
-  font-family: 'Comfortaa', cursive;
+  font-family: 'Comfortaa', sans-serif;
   color: #fffcee;
   font-size: 20px;
 `;
