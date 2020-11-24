3b98343c4f687e0a64b16dcc0c973a27a0fd07f5
Marco Bruno <marco.bruno.br@gmail.com>
2020-11-24 11:04:26 +0100

    ✨ Add the ServiceFilter in storybook

---
+++ src/stories/components/ServiceFilter.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ServiceFilter from '../../components/ServiceFilter';
+
+export default {
+  title: 'Components/SerivceFilter',
+  component: ServiceFilter
+};
+
+export const Default = () => <ServiceFilter />;
