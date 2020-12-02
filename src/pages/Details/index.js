60349132b2aa5e203b4dbacf3107f1f9ce8a26d2
Marco Bruno <marco.bruno.br@gmail.com>
2020-12-02 15:09:46 +0100

    ✨ Create the Title component

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import BackAction from '../../components/BackAction';
 import HeaderMain from '../../components/HeaderMain';
+import Stars from '../../components/Stars';
+import Title from '../../components/Title';
 
 function Details() {
   return (
     <>
       <HeaderMain />
       <BackAction />
+      <Stars amount="5" />
+      <Title>Cafeteria D’ Arte</Title>
     </>
   );
 }
