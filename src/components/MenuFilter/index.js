0b894b1ea4356c89144cf6f5c4a4c150424cfb1a
Marco Bruno <marco.bruno.br@gmail.com>
2020-12-04 17:15:18 +0100

    ✨ Add props in storybok of the MenuFilter

--- src/components/MenuFilter/index.js
+++ src/components/MenuFilter/index.js
@@ -2,23 +2,12 @@ import React from 'react';
 import Checkbox from '../Checkbox';
 import MenuFilterStyle from './styles';
 
-function MenuFilter({ active }) {
+function MenuFilter({ active, items }) {
   return (
     <MenuFilterStyle active={active}>
-      <Checkbox id="alimentacao" label="Alimentção" />
-      <Checkbox id="animais" label="Animais" />
-      <Checkbox id="arteecultura" label="Arte e cultura" />
-      <Checkbox id="entidades" label="Entidades" />
-      <Checkbox id="autioevideo" label="Audio e vídeo" />
-      <Checkbox id="automovies" label="Automovies" />
-      <Checkbox id="bancos" label="Bancos" />
-      <Checkbox id="baresebebidas" label="Bares e bebidas" />
-      <Checkbox id="belezaeestetica" label="Beleza e estética" />
-      <Checkbox id="cafes" label="Cafés" />
-      <Checkbox id="criancaebebe" label="Crianças e bêbes" />
-      <Checkbox id="diversaoelazer" label="Diversão e lazer" />
-      <Checkbox id="escritorio" label="Escritóros" />
-      <Checkbox id="esporte" label="Esporte" />
+      {items.map(({ id, label }) => (
+        <Checkbox id={id} label={label} />
+      ))}
     </MenuFilterStyle>
   );
 }
