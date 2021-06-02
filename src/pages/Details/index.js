5a68898c78e96f0f4c037920f100c3060094bc14
Marco Bruno <marco.bruno.br@gmail.com>
2021-06-02 15:22:53 +0200

    ✨Add the Week component in Details page

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -1,27 +1,92 @@
 import React from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import BackAction from '../../components/BackAction';
-import Day from '../../components/Day';
 import HeaderMain from '../../components/HeaderMain';
 import Title from '../../components/Title';
+import Week from '../../components/Week';
 
 import WrapperBusiness from '../../components/WrapperBusiness';
 
 function Details() {
   const history = useHistory();
+  const days = [
+    {
+      title: 'Segunda',
+      hours: [
+        { start: '09:00', end: '14:00' },
+        { start: '16:00', end: '22:00' }
+      ],
+      close: '',
+      noBorder: false
+    },
+    {
+      title: 'Terça',
+      hours: [
+        { start: '09:00', end: '14:00' },
+        { start: '16:00', end: '22:00' }
+      ],
+      close: '',
+      noBorder: false
+    },
+    {
+      title: 'Quarta',
+      hours: [
+        { start: '09:00', end: '14:00' },
+        { start: '16:00', end: '22:00' }
+      ],
+      close: '',
+      noBorder: false
+    },
+    {
+      title: 'Quinta',
+      hours: [
+        { start: '09:00', end: '14:00' },
+        { start: '16:00', end: '22:00' }
+      ],
+      close: '',
+      noBorder: false
+    },
+    {
+      title: 'Sexta',
+      hours: [
+        { start: '09:00', end: '14:00' },
+        { start: '16:00', end: '22:00' }
+      ],
+      close: '',
+      noBorder: false
+    },
+    {
+      title: 'Sábado',
+      hours: [
+        { start: '09:00', end: '14:00' },
+        { start: '16:00', end: '22:00' }
+      ],
+      close: '',
+      noBorder: false
+    },
+    ,
+    {
+      title: 'Domingo',
+      hours: [
+        { start: '09:00', end: '14:00' },
+        { start: '16:00', end: '22:00' }
+      ],
+      close: '',
+      noBorder: false
+    },
+    {
+      title: 'Feriado',
+      hours: '',
+      close: 'Fechado',
+      noBorder: true
+    }
+  ];
 
   const handleClick = () => {
     console.log('enter here => BackAction');
     history.goBack();
   };
 
-  const day = {
-    title: 'Segunda',
-    hours: [
-      { start: '09:00', end: '14:00' },
-      { start: '16:00', end: '22:00' }
-    ]
-  };
   return (
     <>
       <HeaderMain />
@@ -32,8 +97,10 @@ function Details() {
         description="Café do Zé é feliz... é gostoso ... é do povo"
       />
 
-      <Title mantis={true}>Aberto agora</Title>
-      <Day {...day} />
+      <Title mantis={true} gapLeft gapBottom gapTop>
+        Aberto agora
+      </Title>
+      <Week days={days} />
     </>
   );
 }
